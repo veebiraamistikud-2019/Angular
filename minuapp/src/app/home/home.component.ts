47f0fb442b281195adfac1bc77be68b2c3c798de
Tim Jaanson <jaanson.tim@gmail.com>
2019-10-06 21:42:32 +0300

    initial example

---
+++ minuapp/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style: boolean = false;
+  
+  firstClick() {
+    this.h1Style = true;
+  }
+
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
