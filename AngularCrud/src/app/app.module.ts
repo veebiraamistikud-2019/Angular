35ae795ef22df4846dbe6372f1a00b7b0002a398
DENNIS_RICHARD šULGA <desulg@tlu.ee>
2019-11-10 21:54:21 +0200

    Pre-API state

--- AngularCrud/src/app/app.module.ts
+++ AngularCrud/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductGetComponent } from './product-get/product-get.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductsService } from './products.service';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +22,11 @@ import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ProductsService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- AngularCrud/src/app/product-add/product-add.component.ts
+++ AngularCrud/src/app/product-add/product-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-add',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
-
-  constructor() { }
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder, private ps: ProductsService) {
+    this.createForm();
+   }
+   createForm() {
+    this.angForm = this.fb.group({
+      ProductName: ['', Validators.required ],
+      ProductDescription: ['', Validators.required ],
+      ProductPrice: ['', Validators.required ]
+    });
+  }
+  addProduct(ProductName, ProductDescription, ProductPrice) {
+    this.ps.addProduct(ProductName, ProductDescription, ProductPrice);
+  }
 
   ngOnInit() {
   }
